83f898ece78b2ada1c26a272fb49ab99a86989d8
Paczeika Dan <dan.alexandru96@gmail.com>
2018-10-23 14:09:03 +0300

    Initial Commit

---
+++ ShopLeave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopLeave
+//
+//  Created by Paczeika Dan on 23/10/2018.
+//  Copyright © 2018 Paczeika Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
